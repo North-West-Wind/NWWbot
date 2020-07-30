efd7fc1520236d55a8d7ba596a534817d47c1403
NorthWestWind <wsyn148@gmail.com>
2020-07-30 16:16:37 +0800

    Returned to basics

--- api/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res) => {
-  res.sendStatus(200);
-  require("child_process").exec("node ./app.js");
-}
--- index.js
+++ index.js
@@ -1,32 +1,4 @@
-const express = require("express");
-const request = require("request");
-const device = require("express-device");
-const app = express();
-const moment = require("moment");
-app.use(device.capture());
-app.get("/", (req, response) => {
-  if(req.device.type === "phone")
-    response.sendFile(__dirname + "/api/views/mobile/index.html");
-  else
-    response.sendFile(__dirname + "/api/views/index.html");
-});
-app.get("/news", (req, response) => {
-  if(req.device.type === "phone")
-    response.sendFile(__dirname + "/api/views/mobile/news.html");
-  else
-    response.sendFile(__dirname + "/api/views/news.html");
-});
-app.get("/about", (req, response) => {
-  if(req.device.type === "phone")
-    response.sendFile(__dirname + "/api/views/mobile/about.html");
-  else
-    response.sendFile(__dirname + "/api/views/about.html");
-});
-app.get("/manual", (req, response) => {
-  request("https://cdn.glitch.com/0ee8e202-4c9f-43f0-b5eb-2c1dacae0079%2Fmanual.pdf?v=1589543070522").pipe(response);
-});
-app.get("/ping", (req, response) => {
-  console.log(`Pinged at ${moment().format("HH:mm:ss")}`);
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
+module.exports = (req, res) => {
+  res.sendStatus(200);
+  require("child_process").exec("node ./app.js");
+}
