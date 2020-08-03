32da60e2dbd36661c43fea5f00730602d3bcb311
NorthWestWind <wsyn148@gmail.com>
2020-08-03 22:04:01 +0800

    Added the Forgotten Moment

--- handler.js
+++ handler.js
@@ -23,6 +23,9 @@ const profile = (str) => {
 		require("mojang-api").profile(str, function (err, res) { resolve(err ? undefined : res) });
 	})
 }
+const moment = require("moment");
+const formatSetup = require("moment-duration-format");
+formatSetup(moment);
 module.exports = {
     async ready(client, id) {
         console.log(`[${id}] Ready!`);
