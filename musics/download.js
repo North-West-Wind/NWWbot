91c8813bd0bd5e5f2bd2fa63fa3b2541ed47a2eb
NorthWestWind <wsyn148@gmail.com>
2020-08-07 21:16:13 +0800

    Download Error Handling

--- musics/download.js
+++ musics/download.js
@@ -1,17 +1,29 @@
 const ytdl = require("ytdl-core-discord");
 
+const requestStream = url => {
+    return new Promise(resolve => {
+        request(url, (err, res) => resolve(res));
+    });
+};
+const GET = url => {
+    return new Promise(resolve => {
+        http.get(url, res => resolve(res));
+    });
+}
 const Discord = require("discord.js");
 module.exports = {
     name: "download",
     description: "Download the soundtrack that is currently playing on the server.",
     usage: " ",
+    aliases: ["dl"],
     async music(message, serverQueue) {
-        if(!serverQueue) return message.channel.send("There is nothing playing.");
-        if(!serverQueue.songs) serverQueue.songs = [];
-        if(serverQueue.songs.length < 1) return message.channel.send("There is nothing in the song queue.");
+        if (!serverQueue) return message.channel.send("There is nothing playing.");
+        if (!serverQueue.songs) serverQueue.songs = [];
+        if (serverQueue.songs.length < 1) return message.channel.send("There is nothing in the song queue.");
+        let msg = await message.channel.send("Downloading...");
         let song = serverQueue.songs[0];
         let stream;
-        switch(song.type) {
+        switch (song.type) {
             case 2:
                 stream = await requestStream(song.url);
                 break;
@@ -20,16 +32,20 @@ module.exports = {
                 stream = await requestStream(res.responseUrl);
             default:
                 stream = await ytdl(song.url, {
-                  highWaterMark: 1 << 28, requestOptions: {
-                    headers: {
-                      cookie: process.env.COOKIE
+                    highWaterMark: 1 << 28, requestOptions: {
+                        headers: {
+                            cookie: process.env.COOKIE
+                        }
                     }
-                  }
                 });
                 break;
         }
-        if(!stream) return message.reply("there was an error trying to download the soundtrack!");
+        if (!stream) return msg.edit(`<@${message.author.id}>, there was an error trying to download the soundtrack!`);
         let attachment = new Discord.MessageAttachment(stream, `${song.title}.mp3`);
-        message.channel.send("Download Finished", attachment);
+        try {
+            msg.edit("Download Finished", attachment);
+        } catch(err) {
+            msg.edit(`<@${message.author.id}>, there was an error trying to send the soundtrack!`);
+        }
     }
 }
