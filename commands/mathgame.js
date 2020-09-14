4a415f1ade91aed96b8a93bee8751caa2d394afe
NorthWestWind <wsyn148@gmail.com>
2020-09-14 23:12:14 +0800

    Remove some checking

--- commands/mathgame.js
+++ commands/mathgame.js
@@ -10,22 +10,24 @@ module.exports = {
     description: "Math Game prototype.",
     usage: " ",
     async execute(message, args) {
+        /*
         if(args[0] && args[0].toLowerCase() === "clear" && message.author.id === process.env.DC) {
             console.mathgames.clear();
             return;
         }
         for (const arrs of Array.from(console.mathgames.values())) if (arrs.includes(message.author.id)) return message.channel.send("You are already in another game!");
+        */
         var msg = await message.channel.send("Who will be playing this game? (Please mention them)");
         let collected = await msg.channel.awaitMessages(x => x.author.id === message.author.id, { time: 30000, max: 1, errors: ["time"] });
         if (!collected || !collected.first() || !collected.first().content) return msg.edit("You didn't answer me within 30 seconds! Please try again.");
-        await collected.first().delete();
+        await collected.first().delete().catch(() => console.error("Cannot delete message"));
         var players = [message.author];
         var scores = {};
         scores[message.author.id] = 0;
-        for (const arrs of Array.from(console.mathgames.values()))
+        //for (const arrs of Array.from(console.mathgames.values()))
             for (const user of Array.from(collected.first().mentions.users.values()))
-                if (arrs.includes(user.id)) return msg.edit(`**${user.tag}** is already in another game!`);
-                else if (user.id !== message.author.id) { players.push(user); scores[user.id] = 0; }
+                //if (arrs.includes(user.id)) return msg.edit(`**${user.tag}** is already in another game!`);
+                /*else */if (user.id !== message.author.id) { players.push(user); scores[user.id] = 0; }
         let mode = await this.selectMode(message, args);
         var questions = -1;
         var time = -1;
@@ -37,7 +39,7 @@ module.exports = {
                 msg = await msg.edit("Please enter the amount of questions.");
                 collected = await msg.channel.awaitMessages(x => x.author.id === message.author.id, { time: 30000, max: 1, errors: ["time"] });
                 if(!collected || !collected.first() || !collected.first().content) return msg.edit("Timed out. Please try again.");
-                await collected.first().delete();
+                await collected.first().delete().catch(() => console.error("Cannot delete message"));
                 questions = parseInt(collected.first().content);
                 if(!questions || questions === 0 | isNaN(questions)) return msg.edit("That's not a valid number!");
                 break;
@@ -45,7 +47,7 @@ module.exports = {
                 msg = await msg.edit("Please enter the time allowed.");
                 collected = await msg.channel.awaitMessages(x => x.author.id === message.author.id, { time: 30000, max: 1, errors: ["time"] });
                 if(!collected || !collected.first() || !collected.first().content) return msg.edit("Timed out. Please try again.");
-                await collected.first().delete();
+                await collected.first().delete().catch(() => console.error("Cannot delete message"));
                 time = ms(collected.first().content);
                 if(!time || time === 0) return msg.edit("That's not a valid number!");
         }
@@ -63,7 +65,7 @@ module.exports = {
         if (collected.first().count - 1 < players.length) return msg.edit({ content: "Someone is not active!", embed: null });
         await msg.reactions.removeAll();
         let now = Date.now();
-        console.mathgames.set(now, players.map(x => x.id));
+        //console.mathgames.set(now, players.map(x => x.id));
         var questionCount = 0;
         var running = true;
         if(time > 0) setTimeout(() => running = false, time);
@@ -74,12 +76,12 @@ module.exports = {
             var correct = false;
             while(!correct) {
                 collected = undefined;
-                collected = await msg.channel.awaitMessages(x => players.map(u => u.id).includes(x.author.id), { time: questions < 0 && time < 0 ? 11000 : 60000 * 5, max: 1, errors: ["time"] });
+                collected = await msg.channel.awaitMessages(x => players.map(u => u.id).includes(x.author.id), { time: questions < 0 && time < 0 ? 15000 : 60000 * 5, max: 1, errors: ["time"] });
                 if(!collected || !collected.first() || !collected.first().content) {
                     running = false;
                     break;
                 }
-                await collected.first().delete();
+                await collected.first().delete().catch(() => console.error("Cannot delete message"));
                 if(parseInt(collected.first().content) === generated.answer) {
                     scores[collected.first().author.id] += 1;
                     correct = true;
@@ -100,7 +102,7 @@ module.exports = {
         .setTimestamp()
         .setFooter("Have a nice day! :)", message.client.user.displayAvatarURL());
         msg.edit({ content: "", embed: em });
-        console.mathgames.delete(now);
+        //console.mathgames.delete(now);
     },
     async selectMode(message) {
         var em1 = new Discord.MessageEmbed()
@@ -118,7 +120,7 @@ module.exports = {
         var em3 = new Discord.MessageEmbed()
             .setColor(color)
             .setTitle("Endless Mode")
-            .setDescription("In this mode, you can answer **endless amount of questions**\nThe maximum time allowed for each question is **10 seconds + 1 second Network Buffer**\nAnswers are all integers\nQuestions will get **harder** as the game goes")
+            .setDescription("In this mode, you can answer **endless amount of questions**\nThe maximum time allowed for each question is **10 seconds + 5 seconds Network Buffer**\nAnswers are all integers\nQuestions will get **harder** as the game goes")
             .setTimestamp()
             .setFooter("React with ✅ to choose this one.");
         const allEmbeds = [em1, em2, em3];
