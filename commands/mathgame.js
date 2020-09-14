5e244bb05392cd7da87a97ba73417823e71eff48
NorthWestWind <wsyn148@gmail.com>
2020-09-14 18:39:03 +0800

    Remove Reactions

--- commands/mathgame.js
+++ commands/mathgame.js
@@ -17,11 +17,11 @@ module.exports = {
         await collected.first().delete();
         var players = [message.author];
         var scores = {};
-        scores[message.author] = 0;
+        scores[message.author.id] = 0;
         for (const arrs of Array.from(console.mathgames.values()))
             for (const user of Array.from(collected.first().mentions.users.values()))
                 if (arrs.includes(user.id)) return msg.edit(`**${user.tag}** is already in another game!`);
-                else if (user.id !== message.author.id) { players.push(user); scores[user] = 0; }
+                else if (user.id !== message.author.id) { players.push(user); scores[user.id] = 0; }
         let mode = await this.selectMode(message, args);
         var questions = -1;
         var time = -1;
@@ -57,6 +57,7 @@ module.exports = {
         collected = await msg.awaitReactions((reaction, user) => reaction.emoji.name === '👌🏻' && players.map(x => x.id).includes(user.id), { time: 60000, maxUsers: players.length, errors: ["time"] });
         if (!collected || !collected.first()) return msg.edit({ content: "Seriously? No one reacted?", embed: null });
         if (collected.first().count - 1 < players.length) return msg.edit({ content: "Someone is not active!", embed: null });
+        await msg.reactions.removeAll();
         let now = Date.now();
         console.mathgames.set(now, players.map(x => x.id));
         var questionCount = 0;
@@ -91,7 +92,7 @@ module.exports = {
             else if(scores[user.id] === winner.score) winner.name += ` ${user.tag}`;
         }
         em.setTitle("Game Over")
-        .setDescription(`Here are the final scores!\n${players.map(x => `**${x.tag}** --- **${scores[x.id]}**\n`)}\nCongratulations to **${winner.name}** for winning this game with **${winner.score} points**!`)
+        .setDescription(`Here are the final scores after **${moment.duration(Math.round((Date.now() - now) / 1000), "seconds").format()}**!\n${players.map(x => `**${x.tag}** --- **${scores[x.id]}**\n`)}\nCongratulations to **${winner.name}** for winning this game with **${winner.score} points**!`)
         .setTimestamp()
         .setFooter("Have a nice day! :)", message.client.user.displayAvatarURL());
         msg.edit({ content: "", embed: em });
