b46062ee73a759f3b12df48abf43616f6601eb5a
NorthWestWind <wsyn148@gmail.com>
2020-09-30 16:23:21 +0800

    Color splash

--- function.js
+++ function.js
@@ -311,5 +311,26 @@ module.exports = {
         }
     }
     return array[Math.floor(Math.random() * array.length)];
-	}
+  },
+  hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+  
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  },
+  decimalToRgb(decimal) {
+    return {
+      r: (decimal >> 16) & 0xff,
+      g: (decimal >> 8) & 0xff,
+      b: decimal & 0xff,
+    };
+  }
 };
