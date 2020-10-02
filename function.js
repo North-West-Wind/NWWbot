11482677cb4ff151425b5ce41f6acb4bf7905887
NorthWestWind <wsyn148@gmail.com>
2020-10-02 15:53:43 +0800

    Better ms

--- function.js
+++ function.js
@@ -335,18 +335,22 @@ module.exports = {
   readableDate(date) {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear}`;
   },
-  ms(str) {
-    var mses = [];
-    let temp = "";
-    for (let i = 0; i < str.length; i++) {
-      let char = str.substr(i, 1);
-      temp += char;
-      if (!/\d/.test(char)) {
-        mses.push(ms(temp));
-        temp = "";
+  ms(val, options) {
+    if (typeof val === "string" && superms(val) === undefined) {
+      var mses = [];
+      let temp = "";
+      let last = "";
+      for (let i = 0; i < val.length; i++) {
+        let char = val.substr(i, 1);
+        if (!/\d/.test(last) && /\d/.test(char) && i != 0) {
+          if (superms(temp) === undefined) return undefined;
+          mses.push(superms(temp));
+          temp = "";
+        }
+        temp += char;
+        if (val[i + 1] === undefined) mses.push(superms(temp));
       }
-    }
-
-    return mses.reduce((acc, c) => acc + c);
+      return mses.reduce((acc, c) => acc + c);
+    } else return superms(val, options);
   }
 };
