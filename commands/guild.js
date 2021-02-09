1afba1663362ab9b9308a7c111b27a461a874204
NorthWestWind <wsyn148@gmail.com>
2021-02-09 08:12:13 +0800

    Splitted points

--- commands/guild.js
+++ commands/guild.js
@@ -476,35 +476,37 @@ module.exports = {
 		if (!stars) stars = 0;
 		if (!fkdr) fkdr = 0;
 
-		var points = 0;
-		if (maxSa > 35) points = Infinity;
-		else if (maxSa > 30) points += 4;
-		else if (maxSa > 25) points += 3;
-		else if (maxSa > 20) points += 2;
+		var bwpt = 0;
+		var sbpt = 0;
+		if (maxSa > 35) sbpt = Infinity;
+		else if (maxSa > 30) sbpt += 4;
+		else if (maxSa > 25) sbpt += 3;
+		else if (maxSa > 20) sbpt += 2;
 
-		if (maxCatacomb > 28) points = Infinity;
-		else if (maxCatacomb > 25) points += 4;
-		else if (maxCatacomb > 20) points += 3;
-		else if (maxCatacomb > 15) points += 2;
+		if (maxCatacomb > 28) sbpt = Infinity;
+		else if (maxCatacomb > 25) sbpt += 4;
+		else if (maxCatacomb > 20) sbpt += 3;
+		else if (maxCatacomb > 15) sbpt += 2;
 		
-		if (maxSlayer > 500000) points = Infinity;
-		else if (maxSlayer > 300000) points += 4;
-		else if (maxSlayer > 120000) points += 3;
-		else if (maxSlayer > 60000) points += 2;
+		if (maxSlayer > 500000) sbpt = Infinity;
+		else if (maxSlayer > 300000) sbpt += 4;
+		else if (maxSlayer > 120000) sbpt += 3;
+		else if (maxSlayer > 60000) sbpt += 2;
 		
-		if (stars > 200) points = Infinity;
-		else if (stars > 150) points += 4;
-		else if (stars > 100) points += 3;
-		else if (stars > 50) points += 2;
+		if (stars > 200) bwpt = Infinity;
+		else if (stars > 150) bwpt += 4;
+		else if (stars > 100) bwpt += 3;
+		else if (stars > 50) bwpt += 2;
 		
-		if (fkdr > 3) points = Infinity;
-		else if (fkdr > 2) points += 6;
-		else if (fkdr > 1) points += 3;
+		if (fkdr > 3) bwpt = Infinity;
+		else if (fkdr > 2) bwpt += 6;
+		else if (fkdr > 1) bwpt += 3;
+		var points = sbpt + bwpt;
 
 		const result = new Discord.MessageEmbed()
 		.setTitle(`Points of ${args[1]}`)
 		.setColor(console.color())
-		.setDescription(`Total Points: **${points}**${points == Infinity ? " (Instant Accept!)" : ""}`)
+		.setDescription(`Total Points: **${points}**${points == Infinity ? " (Instant Accept!)" : ""}\nSkyBlock Points: **${sbpt}**\nBedWars Points: **${bwpt}**`)
 		.addField("Average Skill Level", maxSa, true)
 		.addField("Catacomb Level", maxCatacomb, true)
 		.addField("Slayer EXP", maxSlayer, true)
