1e264e192ec78da06eff193dedfb3b842863d34d
NorthWestWind <wsyn148@gmail.com>
2021-02-19 16:33:21 +0800

    Friend

--- handler.js
+++ handler.js
@@ -707,6 +707,7 @@ module.exports = {
                     }
                     const gInfo = await fetch(`https://api.slothpixel.me/api/guilds/${mcUuid}?key=${process.env.API}`).then(res => res.json());
                     if (gInfo.id === "5b25306a0cf212fe4c98d739") await message.member.roles.add("622319008758104064");
+                    else await message.member.roles.add("676754719120556042");
                     if (res.rank === "ADMIN") await message.member.roles.add("649556742434324491");
                     else if (res.rank === "MOD") await message.member.roles.add("649556742832783369");
                     else if (res.rank === "HELPER") await message.member.roles.add("649556743294156810");
