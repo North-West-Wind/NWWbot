8b02423c12bc32c7e594086e2ff8d8de4b0de4d9
NorthWestWind <wsyn148@gmail.com>
2021-02-19 23:38:29 +0800

    Roling everyone

--- commands/role.js
+++ commands/role.js
@@ -29,16 +29,27 @@ module.exports = {
       }
     }
 
-    if (!role) return message.channel.send("No role was found!")
-    let member = await findMember(message, args[0]);
-    if (!member) return;
-    const taggedUser = member.user;
-    member.roles.add(role).then(() => {
-      console.log(`Gave ${taggedUser.username} the role ${role.name}`);
-      message.channel.send(`Successfully added **${taggedUser.tag}** to role **${role.name}**.`);
-
-    }).catch(err => {
-      message.channel.send(`Failed adding **${taggedUser.tag}** to role **${role.name}**. (Error: **${err.message}**)`);
-    });
+    if (!role) return message.channel.send("No role was found!");
+    if (args[0] === "@everyone") {
+      await message.channel.send("Warning: Large servers might take a while!");
+      const allMembers = await message.guild.members.fetch();
+      for (const member of allMembers.values()) try {
+        await member.roles.add(role);
+      } catch (err) {
+        await message.channel.send(`Failed to add **${member.user.tag}** to role **${role.name}**. (Error: **${err.message}**)`);
+      }
+      await message.channel.send(`Finished adding everyone to the role **${role.name}**.`);
+    } else {
+      let member = await findMember(message, args[0]);
+      if (!member) return;
+      const taggedUser = member.user;
+      try {
+        await member.roles.add(role);
+        console.log(`Gave ${taggedUser.username} the role ${role.name}`);
+        await message.channel.send(`Successfully added **${taggedUser.tag}** to role **${role.name}**.`);
+      } catch (err) {
+        await message.channel.send(`Failed to add **${taggedUser.tag}** to role **${role.name}**. (Error: **${err.message}**)`);
+      }
+    }
   },
 };
