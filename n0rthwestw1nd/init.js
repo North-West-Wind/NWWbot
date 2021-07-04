c30f55164891633f3a2eee6aeefeebf1fa670c44
NorthWestWind <wsyn148@gmail.com>
2021-07-04 14:12:06 +0800

    Update init.js

--- n0rthwestw1nd/init.js
+++ n0rthwestw1nd/init.js
@@ -20,22 +20,22 @@ const mysql_config = {
 };
 
 module.exports = (client) => {
-    const fontFiles = fs.readdirSync("./fonts").filter(file => file.endsWith(".ttf") && file.startsWith("NotoSans"));
-    for (const file of fontFiles) registerFont(`./fonts/${file}`, { family: "NotoSans", style: file.split(/[\-\.]/)[1].toLowerCase() });
-    registerFont("./fonts/FreeSans.ttf", { family: "free-sans" });
+    const fontFiles = fs.readdirSync("../fonts").filter(file => file.endsWith(".ttf") && file.startsWith("NotoSans"));
+    for (const file of fontFiles) registerFont(`../fonts/${file}`, { family: "NotoSans", style: file.split(/[\-\.]/)[1].toLowerCase() });
+    registerFont("../fonts/FreeSans.ttf", { family: "free-sans" });
 
     for (let i = 0; i < 4; i++) for (let s = 0; s < 13; s++) NorthClient.storage.card.set(twoDigits(i) + twoDigits(s), new Card(i, s));
     NorthClient.storage.card.set("0413", new Card(4, 13));
     NorthClient.storage.card.set("0414", new Card(4, 14));
 
-    const commandFiles = deepReaddir("./commands").filter(file => file.endsWith(".js"));
-    const itemFiles = fs.readdirSync("./items").filter(file => file.endsWith(".js"));
+    const commandFiles = deepReaddir("../commands").filter(file => file.endsWith(".js"));
+    const itemFiles = fs.readdirSync("../items").filter(file => file.endsWith(".js"));
     for (const file of commandFiles) {
         const command = require(file);
         if (command.description) NorthClient.storage.commands.set(command.name, command);
     }
     for (const file of itemFiles) {
-        const item = require(`./items/${file}`);
+        const item = require(`../items/${file}`);
         NorthClient.storage.items.set(item.name.toLowerCase(), item);
     }
     var pool = mysql.createPool(mysql_config).promise();
