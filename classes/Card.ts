6cff8c0ee8c9f9bbf759a423334103f6b9cf2638
NorthWestWind <wsyn148@gmail.com>
2021-07-19 16:05:18 +0800

    Compat with pr

--- classes/Card.ts
+++
@@ -1,9 +0,0 @@
-export class Card {
-constructor(c: number, n: number) {
-    this.color = c;
-    this.number = n;
-}
-
-    color: number;
-    number: number;
-}
--- classes/NorthMessage.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "discord.js";
-import { Pool } from "mysql2/promise";
-import { NorthClient } from "./NorthClient";
-
-export class NorthMessage extends Message {
-    prefix: string;
-    pool: Pool;
-    client: NorthClient;
-}
