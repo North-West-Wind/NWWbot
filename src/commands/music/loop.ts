492faa95fa650ab0ef374054cceea1098754b2ad
NorthWestWind <wsyn148@gmail.com>
2021-09-14 17:11:34 +0800

    New msgOrRes usage

--- src/commands/music/loop.ts
+++ src/commands/music/loop.ts
@@ -22,18 +22,16 @@ class LoopCommand implements SlashCommand {
         var serverQueue = getQueues().get(message.guild.id);
         if (!serverQueue || !serverQueue.songs || !Array.isArray(serverQueue.songs)) serverQueue = setQueue(message.guild.id, [], false, false);
         serverQueue.looping = !serverQueue.looping;
-        var useEdit = false;
         if (serverQueue.repeating && serverQueue.looping) {
             serverQueue.repeating = false;
             await msgOrRes(message, "Disabled repeating to prevent conflict.");
-            useEdit = true;
         }
         try {
             updateQueue(message.guild.id, serverQueue, true);
-            if (serverQueue.looping) await msgOrRes(message, "The queue is now being looped.", useEdit ? "followUp" : false);
-            else await msgOrRes(message, "The queue is no longer being looped.", useEdit ? "followUp" : false);
+            if (serverQueue.looping) await msgOrRes(message, "The queue is now being looped.");
+            else await msgOrRes(message, "The queue is no longer being looped.");
         } catch (err: any) {
-            await msgOrRes(message, "There was an error trying to update the status!", useEdit ? "followUp" : false);
+            await msgOrRes(message, "There was an error trying to update the status!");
         }
     }
 }
--- src/commands/music/repeat.ts
+++ src/commands/music/repeat.ts
@@ -22,18 +22,16 @@ class RepeatCommand implements SlashCommand {
         var serverQueue = getQueues().get(message.guild.id);
         if (!serverQueue || !serverQueue.songs || !Array.isArray(serverQueue.songs)) serverQueue = setQueue(message.guild.id, [], false, false);
         serverQueue.repeating = !serverQueue.repeating;
-        var useEdit = false;
         if (serverQueue.repeating && serverQueue.looping) {
             serverQueue.looping = false;
             await msgOrRes(message, "Disabled looping to prevent conflict.");
-            useEdit = true;
         }
         try {
             updateQueue(message.guild.id, serverQueue);
-            if (serverQueue.repeating) await msgOrRes(message, "The queue is now being repeated.", useEdit ? "followUp" : false);
-            else await msgOrRes(message, "The queue is no longer being repeated.", useEdit ? "followUp" : false);
+            if (serverQueue.repeating) await msgOrRes(message, "The queue is now being repeated.");
+            else await msgOrRes(message, "The queue is no longer being repeated.");
         } catch (err: any) {
-            await msgOrRes(message, "There was an error trying to update the status!", useEdit ? "followUp" : false);
+            await msgOrRes(message, "There was an error trying to update the status!");
         }
     }
 }
