fd4d7035ca7e76ef45920f1251f5226dbcb415bb
NorthWestWind <wsyn148@gmail.com>
2021-09-16 23:27:13 +0800

    Temp disable gtimer

--- src/handler.ts
+++ src/handler.ts
@@ -522,7 +522,7 @@ export class AliceHandler extends Handler {
                 conn.release();
             }, endAfter);
         });
-        const [gtimers] = <RowDataPacket[][]><unknown>await con.query(`SELECT * FROM gtimer ORDER BY endAt ASC`);
+        /*const [gtimers] = <RowDataPacket[][]><unknown>await con.query(`SELECT * FROM gtimer ORDER BY endAt ASC`);
         NorthClient.storage.gtimers = <GuildTimer[]> gtimers;
         setInterval(async () => {
             try {
@@ -614,10 +614,8 @@ export class AliceHandler extends Handler {
                     });
                     collector.on("end", () => msg.reactions.removeAll().catch(() => {}));
                 }
-            } catch (err: any) {
-                console.error(err);
-            }
-        }, 60000);
+            } catch (err: any) {}
+        }, 60000);*/
     }
 
     async readGiveaways(client: NorthClient, con: Connection) {
