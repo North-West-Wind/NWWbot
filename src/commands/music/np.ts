c54365ac5c2adeebf14a738980040e15668c9a69
NorthWestWind <wsyn148@gmail.com>
2021-09-23 23:32:36 +0800

    Update np.ts

--- src/commands/music/np.ts
+++ src/commands/music/np.ts
@@ -4,7 +4,7 @@ import * as moment from "moment";
 import formatSetup from "moment-duration-format";
 formatSetup(moment);
 import * as Discord from "discord.js";
-import ms from "ms";
+import ms, { StringValue } from "ms";
 import { color, msgOrRes } from "../../function";
 import { getQueues, setQueue, updateQueue } from "../../helpers/music";
 import { globalClient as client } from "../../common";
@@ -49,7 +49,7 @@ class NPCommand implements SlashCommand {
         for (let i = 0; i < 20; i++) processBar.push("═");
         var progress = 0;
         const isLive = !!serverQueue?.songs[0]?.isLive;
-        const length = isLive ? 0 : (serverQueue.songs[0].time ? ms(serverQueue.songs[0].time) : 1);
+        const length = isLive ? 0 : (serverQueue.songs[0].time ? ms(<StringValue> serverQueue.songs[0].time) : 1);
         if (isLive) {
             processBar.splice(19, 1, "■");
             var positionTime = "∞";
