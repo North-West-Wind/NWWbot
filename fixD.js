5c68df88badfc0517a552ce4246b89ca6c67041e
NorthWestWind <wsyn148@gmail.com>
2021-11-07 14:06:47 +0800

    Actual file name

--- fixD.js
+++ fixD.js
@@ -1,7 +1,7 @@
 var fs = require('fs')
-fs.readFile("your file", {encoding: 'utf8'}, function (err,data) {
+fs.readFile("./node_modules/discord.js/typings/index.d.ts", {encoding: 'utf8'}, function (err,data) {
     var formatted = data.replace(/private constructor\(client: Client, data: RawMessageData\);/g, 'public constructor(client: Client, data: RawMessageData);');
-fs.writeFile("your file", formatted, 'utf8', function (err) {
+fs.writeFile("./node_modules/discord.js/typings/index.d.ts", formatted, 'utf8', function (err) {
     if (err) return console.log(err);
  });
 });
