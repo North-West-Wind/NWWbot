6e3fd85aaf4f34c9722a06112bf390b4e9fc3edc
NorthWestWind <wsyn148@gmail.com>
2022-02-08 19:46:18 +0800

    fs not require but import

--- fix.js
+++ fix.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+import { readFile, writeFile } from 'fs';
 const ytdlFix = `const ytdl = async (link, options) => {
   const stream = createStream(options);
   return new Promise(async (r, j) => {
@@ -12,16 +12,16 @@ const ytdlFix = `const ytdl = async (link, options) => {
 }
 module.exports = ytdl;`;
 
-fs.readFile("./node_modules/discord.js/typings/index.d.ts", { encoding: 'utf8' }, function (err, data) {
+readFile("./node_modules/discord.js/typings/index.d.ts", { encoding: 'utf8' }, function (err, data) {
     var formatted = data.replace(/private constructor\(client: Client, data: RawMessageData\);/g, 'public constructor(client: Client, data: RawMessageData);');
-    fs.writeFile("./node_modules/discord.js/typings/index.d.ts", formatted, 'utf8', function (err) {
+    writeFile("./node_modules/discord.js/typings/index.d.ts", formatted, 'utf8', function (err) {
         if (err) return console.log(err);
     });
 });
 
-fs.readFile("./node_modules/ytdl-core/lib/index.js", { encoding: "utf8" }, function (_err, data) {
+readFile("./node_modules/ytdl-core/lib/index.js", { encoding: "utf8" }, function (_err, data) {
   var formatted = data.replace(/const ytdl = \((.|\n)*ytdl;/gm, ytdlFix);
-    fs.writeFile("./node_modules/ytdl-core/lib/index.js", formatted, 'utf8', function (err) {
+    writeFile("./node_modules/ytdl-core/lib/index.js", formatted, 'utf8', function (err) {
         if (err) return console.log(err);
     });
 });
