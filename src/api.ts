63b035dfb6659a31ebc9a8602a8e6e1a448c875d
NorthWestWind <wsyn148@gmail.com>
2022-02-15 17:18:36 +0800

    Krunker clan

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import * as mysql from "mysql2";
 import { PoolConnection } from "mysql2/promise";
+import * as DCBots from "discord-user-bots";
+
 dotenv.config();
 
 const mysql_config = {
@@ -18,6 +20,26 @@ var pool = mysql.createPool(mysql_config).promise();
 var con: PoolConnection;
 var conTimeout: NodeJS.Timeout;
 
+const client = new DCBots.Client(process.env.TOKEN_U);
+client.on.message_create = async(message: any) => {
+    if (message.author.id != "717416553099952219") return;
+    const msg = (await client.fetch_messages(2, process.env.CHANNEL_U))[0];
+    if (msg.author.id != client.user.id) return;
+    const [ command, name ] = msg.content.split(" ");
+    switch (<string> command) {
+        case "g.pf":
+            if (message.embeds.length || !message.attachments[0]?.url) profiles[name] = { found: false };
+            else profiles[name] = { found: true, url: message.attachments[0].url };
+            break;
+        case "g.clan":
+            if (message.embeds.length || !message.attachments[0]?.url) clans[name] = { found: false };
+            else clans[name] = { found: true, url: message.attachments[0].url };
+            break;
+    }
+}
+const profiles = {};
+const clans = {};
+
 const app = express();
 
 app.get("/api/:query", async(req, res) => {
@@ -32,6 +54,34 @@ app.get("/api/:query", async(req, res) => {
     } else if (conTimeout) conTimeout.refresh();
     const [results] = <mysql.RowDataPacket[][]> await con.query(req.params.query);
     res.json(results);
+});
+
+app.get("/api/krunker/profile/:username", async(req, res) => {
+    await client.send(process.env.CHANNEL_U, { content: `g.pf ${req.params.username}` });
+    function check() {
+        setTimeout(() => {
+            if (!profiles[req.params.username]) check();
+            else {
+                res.json(profiles[req.params.username]);
+                delete profiles[req.params.username];
+            }
+        }, 200);
+    }
+    check();
+});
+
+app.get("/api/krunker/clan/:name", async (req, res) => {
+    await client.send(process.env.CHANNEL_U, { content: `g.clan ${req.params.name}` });
+    function check() {
+        setTimeout(() => {
+            if (!clans[req.params.name]) check();
+            else {
+                res.json(clans[req.params.name]);
+                delete clans[req.params.name];
+            }
+        }, 200);
+    }
+    check();
 });
 
 app.listen(4269, () => console.log("API Ready!"));
--- src/commands/api/krunker.ts
+++ src/commands/api/krunker.ts
@@ -1,12 +1,9 @@
 import * as Discord from "discord.js";
-import * as DCBots from "discord-user-bots";
-import { color } from "../../function.js";
+import { color, getFetch, msgOrRes, wait } from "../../function.js";
 import { NorthInteraction, NorthMessage, SlashCommand } from "../../classes/NorthClient.js";
 import { run } from "../../helpers/puppeteer.js";
 import { Page } from "puppeteer-core";
 import { globalClient as client } from "../../common.js";
-
-var clientU: DCBots.Client;
 
 class KrunkerCommand implements SlashCommand {
     name = "krunker";
@@ -15,9 +12,9 @@ class KrunkerCommand implements SlashCommand {
     usage = "<subcommand>";
     args = 1;
     category = 7;
-    subcommands = ["stats", "server", "changelog"];
-    subdesc = ["Displays the stats of a Krunker player.", "Shows all available Krunker servers.", "Fetches the changelog of Krunker."];
-    subusage = ["<subcommand> <username>", "<subcommand> [search]", "<subcommand> [version]"];
+    subcommands = ["stats", "clan", "server", "changelog"];
+    subdesc = ["Displays the stats of a Krunker player.", "Display the stats of a Krunker clan.", "Shows all available Krunker servers.", "Fetches the changelog of Krunker."];
+    subusage = ["<subcommand> <username>", "<subcommand> <name>", "<subcommand> [search]", "<subcommand> [version]"];
 
     options = [
         {
@@ -29,6 +26,19 @@ class KrunkerCommand implements SlashCommand {
                     type: "STRING",
                     name: "username",
                     description: "The username of the player.",
+                    required: true
+                }
+            ]
+        },
+        {
+            type: "SUB_COMMAND",
+            name: "clan",
+            description: "Display the stats of a Krunker clan.",
+            options: [
+                {
+                    type: "STRING",
+                    name: "name",
+                    description: "The name of the clan.",
                     required: true
                 }
             ]
@@ -59,56 +69,44 @@ class KrunkerCommand implements SlashCommand {
         }
     ];
 
-    constructor() {
-        if (client.id == 0) this.initClientU();
-    }
-
     async execute(interaction: NorthInteraction) {
+        await interaction.deferReply();
         const sub = interaction.options.getSubcommand();
-        if (sub === "stats") {
-            const msg = <Discord.Message> await interaction.reply({ content: "Loading servers...", fetchReply: true });
-            await this.stats(msg, interaction.options.getString("username"));
-        } else if (sub === "server") {
-            const msg = <Discord.Message> await interaction.reply({ content: "Loading servers...", fetchReply: true });
-            await this.server(msg, interaction.options.getString("search") || null, interaction.user);
-        } else if (sub === "changelog") {
-            const msg = <Discord.Message> await interaction.reply({ content: "Loading changelogs...",  fetchReply: true });
-            await this.changelog(msg, interaction.options.getString("version") || null, interaction.user);
+        switch (interaction.options.getSubcommand()) {
+            case "stats": return await this.stats(interaction, interaction.options.getString("username"));
+            case "clan": return await this.clan(interaction, interaction.options.getString("name"));
+            case "server": return await this.server(interaction, interaction.options.getString("search") || null, interaction.user);
+            case "changelog": return await this.changelog(interaction, interaction.options.getString("version") || null, interaction.user);
         }
     }
 
     async run(message: NorthMessage, args: string[]) {
         switch (args[0]) {
-            case "stats":
-                var msg = await message.channel.send("Loading stats...");
-                await this.stats(msg, args.slice(1).join(" "));
-                break;
-            case "server":
-                var msg = await message.channel.send("Loading servers...");
-                await this.server(msg, args.slice(1).join(" "), message.author);
-                break;
-            case "changelog":
-                var msg = await message.channel.send("Loading changelogs...");
-                await this.changelog(msg, args.slice(1).join(" "), message.author);
-                break;
-            default:
-                var msg = await message.channel.send("Loading stats...");
-                await this.stats(msg, args.join(" "));
+            case "stats": return await this.stats(message, args.slice(1).join(" "));
+            case "clan": return await this.clan(message, args.slice(1).join(" "));
+            case "server": return await this.server(message, args.slice(1).join(" "), message.author);
+            case "changelog": return await this.changelog(message, args.slice(1).join(" "), message.author);
+            default: return await this.stats(message, args.join(" "));
         }
     }
 
-    async stats(msg: Discord.Message, username: string) {
-        await clientU.send(process.env.CHANNEL_U, { content: `${process.env.TOP_SECRET_STRING} ${username}` });
-        const res = await (<Discord.TextChannel> await msg.client.channels.fetch(process.env.CHANNEL_U)).awaitMessages({ max: 1, time: 10000, filter: m => m.author.id == process.env.GBID });
-        if (!res.first()) return await msg.edit("Failed to acquire user stats!");
-        const mesg = res.first();
-        msg.delete().catch(() => { });
-        if (mesg.embeds?.length) return await msg.channel.send("The user does not exist!");
-        if (!mesg.attachments?.size) return await msg.channel.send("Failed to acquire user stats!");
-        await msg.channel.send({ files: [...mesg.attachments.values()] });
+    async stats(message: Discord.Message | Discord.CommandInteraction, username: string) {
+        const res = await getFetch()("http://localhost:4269/api/krunker/profile/" + username);
+        if (!res.ok) return await msgOrRes(message, "Failed to acquire user stats!");
+        const json = <any> await res.json();
+        if (!json.found) return await msgOrRes(message, "The user does not exist!");
+        await msgOrRes(message, { files: [new Discord.MessageAttachment(json.url)] });
     }
 
-    async server(msg: Discord.Message, search: string, author: Discord.User) {
+    async clan(message: Discord.Message | Discord.CommandInteraction, name: string) {
+        const res = await getFetch()("http://localhost:4269/api/krunker/clan/" + name);
+        if (!res.ok) return await msgOrRes(message, "Failed to acquire clan stats!");
+        const json = <any> await res.json();
+        if (!json.found) return await msgOrRes(message, "The clan does not exist!");
+        await msgOrRes(message, { files: [new Discord.MessageAttachment(json.url)] });
+    }
+
+    async server(message: Discord.Message | Discord.CommandInteraction, search: string, author: Discord.User) {
         try {
             const servers = await this.getServers();
             if (servers.error) throw new Error(servers.message);
@@ -121,7 +119,7 @@ class KrunkerCommand implements SlashCommand {
                 official = servers.games.filter(x => (x[4].i.includes(search) || x[0].includes(search)) && !x[4].cs);
                 custom = servers.games.filter(x => (x[4].i.includes(search) || x[0].includes(search)) && x[4].cs);
             }
-            if (official.length < 1 && custom.length < 1) return msg.edit("No server was found!");
+            if (official.length < 1 && custom.length < 1) return await msgOrRes(message, "No server was found!");
             official.sort(function (a, b) {
                 var nameA = a[0];
                 var nameB = b[0];
@@ -165,19 +163,20 @@ class KrunkerCommand implements SlashCommand {
                     .setFooter({ text: `There are ${customPage} pages for custom games.`, iconURL: client.user.displayAvatarURL() });
                 allEmbeds.push(em);
             }
-            await msg.edit({ embeds: [allEmbeds[0]] });
-
             var s = 0;
-            await msg.react("🎲");
-            await msg.react("🔗");
-            await msg.react("⏩");
-            await msg.react("⏮");
-            await msg.react("◀");
-            await msg.react("▶");
-            await msg.react("⏭");
-            await msg.react("⏹");
-            var collector = msg.createReactionCollector({
-                filter: (reaction, user) => (["🎲", "🔗", "⏩", "◀", "▶", "⏮", "⏭", "⏹"].includes(reaction.emoji.name) && user.id === author.id),
+            const row1 = new Discord.MessageActionRow()
+                .addComponents(new Discord.MessageButton({ emoji: "⏮", customId: "first", style: "PRIMARY" }))
+                .addComponents(new Discord.MessageButton({ emoji: "◀", customId: "previous", style: "PRIMARY" }))
+                .addComponents(new Discord.MessageButton({ emoji: "▶", customId: "next", style: "PRIMARY" }))
+                .addComponents(new Discord.MessageButton({ emoji: "⏭", customId: "last", style: "PRIMARY" }))
+                .addComponents(new Discord.MessageButton({ emoji: "⏹", customId: "quit", style: "DANGER" }));
+            const row2 = new Discord.MessageActionRow()
+                .addComponents(new Discord.MessageButton({ label: "Random Game", emoji: "🎲", customId: "random", style: "SECONDARY" }))
+                .addComponents(new Discord.MessageButton({ label: "Random Regional", emoji: "🔗", customId: "region", style: "SECONDARY" }))
+                .addComponents(new Discord.MessageButton({ label: "Page Warp", emoji: "⏩", customId: "warp", style: "SECONDARY" }));
+            const msg = await msgOrRes(message, { embeds: [allEmbeds[0]], components: [row1, row2] });
+            var collector = msg.createMessageComponentCollector({
+                filter: (interaction) => interaction.user.id === author.id,
                 idle: 60000
             });
             const linkEmbed = new Discord.MessageEmbed()
@@ -191,54 +190,53 @@ class KrunkerCommand implements SlashCommand {
                 .setDescription("Enter the page number to warp to that page.")
                 .setTimestamp()
                 .setFooter({ text: "Please decide within 30 seconds.", iconURL: client.user.displayAvatarURL() });
-            collector.on("collect", async function (reaction, user) {
-                reaction.users.remove(user.id).catch(() => {});
-                switch (reaction.emoji.name) {
-                    case "🎲":
-                        if (s > officialPage - 1) msg.channel.send(`https://krunker.io/?game=${custom[Math.floor(Math.random() * custom.length)][0]}`);
-                        else msg.channel.send(`https://krunker.io/?game=${official[Math.floor(Math.random() * official.length)][0]}`);
+            collector.on("collect", async function (interaction: Discord.MessageComponentInteraction) {
+                switch (interaction.customId) {
+                    case "random":
+                        if (s > officialPage - 1) await message.channel.send(`https://krunker.io/?game=${custom[Math.floor(Math.random() * custom.length)][0]}`);
+                        else await message.channel.send(`https://krunker.io/?game=${official[Math.floor(Math.random() * official.length)][0]}`);
                         break;
-                    case "🔗":
+                    case "region":
                         var options = [];
                         if (s > officialPage - 1) options = Array.from(new Set(custom.map(x => x[0].split(":")[0])));
                         else options = Array.from(new Set(official.map(x => x[0].split(":")[0])));
                         linkEmbed.setDescription(`Available regions:\n**${options.join("\n")}**\n\nPlease type the region in the channel.`);
-                        await msg.edit({ embeds: [linkEmbed] });
-                        const collected = await msg.channel.awaitMessages({ filter: m => m.author.id === author.id,  max: 1, time: 30000 });
+                        await interaction.update({ embeds: [linkEmbed], components: [] });
+                        const collected = await message.channel.awaitMessages({ filter: m => m.author.id === author.id, max: 1, time: 30000 });
                         if (collected && collected.first()) await collected.first().delete();
                         if (collected.first().content && options.includes(collected.first().content.split(/ +/)[0].toUpperCase())) {
                             const region = options.find(x => x === collected.first().content.split(/ +/)[0].toUpperCase());
                             var games = [];
                             if (s > officialPage - 1) games = custom.filter(x => x[0].startsWith(region));
                             else games = official.filter(x => x[0].startsWith(region));
-                            msg.channel.send(`https://krunker.io/?game=${games[Math.floor(Math.random() * games.length)][0]}`);
+                            await message.channel.send(`https://krunker.io/?game=${games[Math.floor(Math.random() * games.length)][0]}`);
                         }
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await msg.edit({ embeds: [allEmbeds[s]], components: [row1, row2] });
                         break;
-                    case "⏩":
-                        await msg.edit({ embeds: [pageWarp] });
-                        const collected1 = await msg.channel.awaitMessages({ filter: m => m.author.id === author.id,  max: 1, time: 30000 });
+                    case "warp":
+                        await interaction.update({ embeds: [pageWarp], components: [] });
+                        const collected1 = await msg.channel.awaitMessages({ filter: m => m.author.id === author.id, max: 1, time: 30000 });
                         if (collected1 && collected1.first()) await collected1.first().delete();
                         if (collected1.first().content && !isNaN(parseInt(collected1.first().content))) s = (parseInt(collected1.first().content) - 1) % allEmbeds.length;
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await msg.edit({ embeds: [allEmbeds[s]], components: [row1, row2] });
                         break;
                     case "⏮":
                         s = 0;
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await interaction.update({ embeds: [allEmbeds[s]] });
                         break;
                     case "◀":
                         s -= 1;
                         if (s < 0) s = allEmbeds.length - 1;
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await interaction.update({ embeds: [allEmbeds[s]] });
                         break;
                     case "▶":
                         s += 1;
                         if (s > allEmbeds.length - 1) s = 0;
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await interaction.update({ embeds: [allEmbeds[s]] });
                         break;
                     case "⏭":
                         s = allEmbeds.length - 1;
-                        await msg.edit({embeds: [allEmbeds[s]]});
+                        await interaction.update({ embeds: [allEmbeds[s]] });
                         break;
                     case "⏹":
                         collector.emit("end");
@@ -246,20 +244,23 @@ class KrunkerCommand implements SlashCommand {
                 }
             });
             collector.on("end", async function () {
-                msg.reactions.removeAll().catch(() => {});
-                var random = "";
-                if (s > officialPage - 1) random = (`https://krunker.io/?game=${custom[Math.floor(Math.random() * custom.length)][0]}`);
-                else random = (`https://krunker.io/?game=${official[Math.floor(Math.random() * official.length)][0]}`);
-                if (random.endsWith("undefined")) random = "";
-                setTimeout(() => msg.edit({ content: random.length > 0 ? `Here's a random server:\n${random}` : "No server was found!", embeds: [] }).catch(() => {}), 30000);
+                try {
+                    await msg.edit({ components: [] });
+                    var random = "";
+                    if (s > officialPage - 1) random = (`https://krunker.io/?game=${custom[Math.floor(Math.random() * custom.length)][0]}`);
+                    else random = (`https://krunker.io/?game=${official[Math.floor(Math.random() * official.length)][0]}`);
+                    if (random.endsWith("undefined")) random = "";
+                    await wait(30000);
+                    if (random.length) msg.edit(`Here's a random server:\n<${random}>`).catch(() => { });
+                } catch (err) { }
             });
         } catch (err: any) {
             console.error(err);
-            msg.edit(`<@${author.id}>, there was an error trying to show you the games!`);
+            await msgOrRes(message, `There was an error trying to show you the games!`, true);
         }
     }
 
-    async changelog(msg: Discord.Message, version: string, author: Discord.User) {
+    async changelog(message: Discord.Message | Discord.CommandInteraction, version: string, author: Discord.User) {
         try {
             const changelogs = await this.getChangelog();
             if (changelogs.error) throw new Error(changelogs.message);
@@ -267,14 +268,14 @@ class KrunkerCommand implements SlashCommand {
             changelog[Object.keys(changelogs).find(x => x.includes("UPDATE"))] = changelogs[Object.keys(changelogs).find(x => x.includes("UPDATE"))];
             if (version) {
                 const key = Object.keys(changelogs).find(x => x.includes(version.toUpperCase()));
-                if (!key) return await msg.channel.send("Cannot find any changelog with the supplied string!");
+                if (!key) return await msgOrRes(message, "Cannot find any changelog with the supplied string!");
                 changelog = {};
                 changelog[key] = changelogs[key];
             }
-            await msg.edit(`\`\`\`${Object.keys(changelog)[0]}\n${changelog[Object.keys(changelog)[0]].join("\n")}\`\`\``);
+            await msgOrRes(message, `\`\`\`${Object.keys(changelog)[0]}\n${changelog[Object.keys(changelog)[0]].join("\n")}\`\`\``);
         } catch (err: any) {
             console.error(err);
-            msg.edit(`<@${author.id}>, there was an error trying to display the changelog!`);
+            await msgOrRes(message, `There was an error trying to display the changelog!`, true);
         }
     }
 
@@ -302,7 +303,7 @@ class KrunkerCommand implements SlashCommand {
             try {
                 await page.goto("https://krunker.io/docs/versions.txt");
                 const element = await page.$("pre");
-                const text = <string> await (await element.getProperty("textContent")).jsonValue();
+                const text = <string>await (await element.getProperty("textContent")).jsonValue();
                 for (const x of text.split("\n\n")) {
                     const morelines = x.split("\n");
                     if (morelines[0] === "") morelines.shift();
@@ -314,10 +315,6 @@ class KrunkerCommand implements SlashCommand {
                 return result.error ? result : lines;
             }
         })
-    }
-
-    initClientU() {
-        clientU = new DCBots.Client(process.env.TOKEN_U);
     }
 }
 
