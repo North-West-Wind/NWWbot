a226e177336d3085b593448acc9f59d411fdc1a5
NorthWestWind <wsyn148@gmail.com>
2022-06-03 09:14:38 +0800

    EZ

--- src/handler.ts
+++ src/handler.ts
@@ -682,6 +682,43 @@ export class AliceHandler extends Handler {
         super.messageDelete(message);
     }
 
+    static readonly replacements = [
+        "Wait... This isn't what I typed!",
+        "Anyone else really like Rick Astley?",
+        "Hey helper, how play game?",
+        "Sometimes I sing soppy, love songs in the car.",
+        "I like long walks on the beach and playing Hypixel",
+        "Please go easy on me, this is my first game!",
+        "You're a great person! Do you want to play some Hypixel games with me?",
+        "In my free time I like to watch cat videos on Youtube",
+        "When I saw the witch with the potion, I knew there was trouble brewing.",
+        "If the Minecraft world is infinite, how is the sun spinning around it?",
+        "Hello everyone! I am an innocent player who loves everything Hypixel.",
+        "Plz give me doggo memes!",
+        "I heard you like Minecraft, so I built a computer in Minecraft in your Minecraft so you can Minecraft while you Minecraft",
+        "Why can't the Ender Dragon read a book? Because he always starts at the End.",
+        "Maybe we can have a rematch?",
+        "I sometimes try to say bad things then this happens 😦",
+        "Behold, the great and powerful, my magnificent and almighty nemisis!",
+        "Doin a bamboozle fren.",
+        "Your clicks per second are godly. :eek:",
+        "What happens if I add chocolate milk to macaroni and cheese?",
+        "Can you paint with all the colors of the wind",
+        "Blue is greener than purple for sure",
+        "I had something to say, then I forgot it.",
+        "When nothing is right, go left.",
+        "I need help, teach me how to play!",
+        "Your personality shines brighter than the sun.",
+        "You are very good at the game friend.",
+        "I like pineapple on my pizza",
+        "I like pasta, do you prefer nachos?",
+        "I like Minecraft pvp but you are truly better than me!",
+        "I have really enjoyed playing with you! ❤️",
+        "ILY ❤️",
+        "Pineapple doesn't go on pizza!",
+        "Lets be friends instead of fighting okay?"
+    ];
+
     async message(message: Message) {
         if (message.channel.id == "647630951169523762") {
             if (!message.content.match(/^\w{3,16}$/)) return;
@@ -812,6 +849,10 @@ export class AliceHandler extends Handler {
                     break;
                 }
             }
+        }
+        if (message.content?.toLowerCase() === "ez") {
+            await message.delete();
+            await message.channel.send(`<@${message.id}> said:\n> ${AliceHandler.replacements[Math.floor(Math.random() * AliceHandler.replacements.length)]}`);
         }
         super.message(message);
     }
