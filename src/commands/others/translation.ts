e1e2d515f916af51a7208c3015be56212503974b
NorthWestWind <45654842+North-West-Wind@users.noreply.github.com>
2022-07-02 21:12:45 +0800

    announcement linking system

--- src/commands/others/translation.ts
+++ src/commands/others/translation.ts
@@ -47,6 +47,25 @@ class TranslationCommand implements SlashCommand {
 				required: false,
 				type: "STRING"
 			}]
+		},
+		{
+			name: "link",
+			description: "Links an announcement to the translation.",
+			type: "SUB_COMMAND",
+			options: [
+				{
+					name: "announced_id",
+					description: "The ID of the announcement message.",
+					required: true,
+					type: "STRING"
+				},
+				{
+					name: "translate_id",
+					description: "The ID of the translation message.",
+					required: false,
+					type: "STRING"
+				}
+			]
 		}
 	]
 
@@ -56,6 +75,7 @@ class TranslationCommand implements SlashCommand {
 			case "announce": return await this.announce(interaction);
 			case "end": return await this.end(interaction);
 			case "get": return await this.get(interaction);
+			case "link": return await this.link(interaction);
 			default: await interaction.editReply("how tf did u get here?")
 		}
 	}
@@ -318,6 +338,93 @@ class TranslationCommand implements SlashCommand {
 					const [id, lang] = interaction.values[0].split("_");
 					const translation = NorthClient.storage.guilds[interaction.guildId].translations.get(id).translations.get(lang);
 					await interaction.update({ embeds: [], components: [], content: (await (<TextChannel>await interaction.guild.channels.fetch(translation.channelId)).messages.fetch(translation.messageId)).content });
+					collector.emit("end");
+				}
+			}
+		});
+		collector.on("end", () => { interaction.editReply({ components: [] }); });
+	}
+	
+	async link(interaction: NorthInteraction) {
+		if (!(<Permissions> interaction.member.permissions).has(BigInt(32))) return await interaction.editReply("You don't have the permissions to use this subcommand!");
+		const announced = interaction.options.getString("announced_id");
+		var id: Snowflake;
+		if (id = interaction.options.getString("id")) {
+			const trans = NorthClient.storage.guilds[interaction.guildId].translations.get(id);
+			if (trans) {
+				trans.existingId = announced;
+				NorthClient.storage.guilds[interaction.guildId].translations.set(id, trans);
+				await query(`UPDATE translations SET existing = ${announced} WHERE id = ${id}`);
+				await interaction.editReply(`Announced message with ID ${id}.`);
+			} else await interaction.editReply(`Message with ID ${id} doesn't exist!`);
+			return;
+		}
+		const translations = NorthClient.storage.guilds[interaction.guildId].translations.filter(trans => !trans.existingId);
+		const allEmbeds: MessageEmbed[] = [];
+		const allRows: MessageActionRow[][] = [];
+		const pages = Math.ceil(translations.size / 5);
+		for (let ii = 0; ii < pages; ii++) {
+			const em = new MessageEmbed()
+				.setColor(color())
+				.setTitle(`Choose a message to link to [${ii + 1}/${pages}]`)
+				.setTimestamp()
+				.setFooter({ text: "Use the buttons to navigate pages and choose an option in the select menu." });
+			const menu = new MessageSelectMenu()
+				.setCustomId("message")
+				.setPlaceholder("Select message...");
+			var description = "";
+			for (let jj = 0; jj < 5; jj++) {
+				const translation = translations.at(ii * 5 + jj);
+				if (!translation) break;
+				const msg = await (<TextChannel>await interaction.guild.channels.fetch(translation.channelId)).messages.fetch(translation.messageId);
+				description += `**${translation.messageId}**\n${msg.content.slice(0, 100)}...\n\n`;
+				menu.addOptions({ label: translation.messageId, value: translation.messageId });
+			}
+			if (description) em.setDescription(description);
+			allEmbeds.push(em);
+			allRows.push([new MessageActionRow().addComponents(menu), new MessageActionRow().addComponents(new MessageButton({ customId: "previous", emoji: "◀️", style: "PRIMARY" }), new MessageButton({ customId: "search", label: "Search by ID", emoji: "🔍", style: "SECONDARY" }), new MessageButton({ customId: "next", emoji: "▶️", style: "PRIMARY" }))]);
+		}
+		var s = 0;
+		const msg = <Message>await interaction.editReply({ embeds: [allEmbeds[s]], components: allRows[s] });
+		const collector = msg.createMessageComponentCollector({ filter: (int) => int.user.id === interaction.user.id, idle: 60000 });
+		collector.on("collect", async (interaction: MessageComponentInteraction) => {
+			if (interaction.isButton()) {
+				switch (interaction.customId) {
+					case "previous":
+						s -= 1;
+						if (s < 0) s = allEmbeds.length - 1;
+						interaction.update({ embeds: [allEmbeds[s]], components: allRows[s] });
+						break;
+					case "next":
+						s += 1;
+						if (s > allEmbeds.length - 1) s = 0;
+						interaction.update({ embeds: [allEmbeds[s]], components: allRows[s] });
+						break;
+					case "search":
+						const modal = new Modal()
+							.setCustomId("modal")
+							.setTitle("Search by ID")
+							.addComponents(new MessageActionRow<TextInputComponent>().addComponents(new TextInputComponent().setCustomId("id").setLabel("What is the message ID you are searching for?").setStyle("SHORT")));
+						await interaction.showModal(modal);
+						const received = <ModalSubmitInteraction> await interaction.awaitModalSubmit({ filter: int => int.user.id === interaction.user.id, time: 60000 }).catch(() => null);
+						if (!received) break;
+						id = received.fields.getTextInputValue("id");
+						const trans = NorthClient.storage.guilds[interaction.guildId].translations.get(id);
+						if (!trans) return await received.update({ embeds: [], components: [], content: `The message with ID ${id} doesn't exist!` });
+						trans.existingId = announced;
+						NorthClient.storage.guilds[interaction.guildId].translations.set(id, trans);
+						await query(`UPDATE translations SET existing = ${announced} WHERE id = ${id}`);
+						await received.update({ components: [], content: `Announced message with ID ${id}.` });
+						collector.emit("end");
+				}
+			} else if (interaction.isSelectMenu()) {
+				if (interaction.customId === "message") {
+					id = interaction.values[0];
+					const trans = NorthClient.storage.guilds[interaction.guildId].translations.get(id);
+					trans.existingId = announced;
+					NorthClient.storage.guilds[interaction.guildId].translations.set(id, trans);
+					await query(`UPDATE translations SET existing = ${announced} WHERE id = ${id}`);
+					await interaction.update({ components: [], embeds: [], content: `Announced message with ID ${id}.` });
 					collector.emit("end");
 				}
 			}
